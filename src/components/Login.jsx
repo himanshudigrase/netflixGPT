4847798f6f1267cbec04aa7bbfdc8b6f77d87847
himanshudigrase <himanshudigrase@gmail.com>
2023-12-29 20:09:19 +0530

    preventFormSubmit && validation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
 
   const [isSignIn, setIsSignIn] = useState();
+  const email = useRef(null);
+  const password = useRef(null);
 
   const handleSignIn = () =>{
     setIsSignIn(!isSignIn);
+  }
+
+  const handleLogin = () =>{
+    console.log(email)
+    console.log(password)
+    const msg = checkValidData(email.current.value,password.current.value)
+    console.log(msg);
+
   }
   return (
     <div>
@@ -18,7 +29,7 @@ const Login = () => {
         />
       </div>
 
-      <form className="absolute bg-black text-white w-3/12 p-12 mx-auto right-0 left-0 my-36 bg-opacity-75 rounded-lg">
+      <form onSubmit={(e)=> e.preventDefault()} className="absolute bg-black text-white w-3/12 p-12 mx-auto right-0 left-0 my-36 bg-opacity-75 rounded-lg">
         <h1 className="font-bold text-3xl py-4">{isSignIn? "Sign In" : "Sign Up"}</h1>
         {!isSignIn && <input
           className="w-full p-4 my-4 bg-gray-700 rounded-lg"
@@ -26,16 +37,18 @@ const Login = () => {
           placeholder="Enter your name"
         />}
         <input
+        ref={email}
           className="w-full p-4 my-4 bg-gray-700 rounded-lg"
           type="text"
           placeholder="Enter your email address"
         />
         <input
+        ref={password}
           className="w-full p-4 my-4 bg-gray-700 rounded-lg"
           type="password"
           placeholder="Enter your password"
         />
-        <button className="bg-red-600 text-white w-full p-4 my-6 rounded-lg">
+        <button onClick={handleLogin} className="bg-red-600 text-white w-full p-4 my-6 rounded-lg">
         {isSignIn? "Sign In" : "Sign Up"}
         </button>
         <p onClick={handleSignIn}>{isSignIn ?"New to Netflix? Sign Up":"Already a user? Sign In here" } </p>
---
+++ src/utils/validate.jsx
@@ -0,0 +1,10 @@
+export const checkValidData = (email,password) =>{
+    const emailRegex = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email);
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password);
+
+    if(!emailRegex) return "Email ID is not valid"
+    if(!passwordRegex) return "Password is not valid."
+
+    return null;
+}
